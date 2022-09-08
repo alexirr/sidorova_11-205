50f415b255a55d74d08c6dcc5c5d170ab8143c53
Александра <aleksandra@MacBook-Air-Aleksandra.local>
2022-09-08 18:39:48 +0300

    second task

---
+++ Program.java
@@ -0,0 +1,15 @@
+public class Program {
+	public static void main (String arg[]) {
+		System.out.println("________________________________");
+		System.out.println("|   &   |   &   |   &   |   &  |");
+		System.out.println("________________________________");
+		System.out.println("|  True | False | True  | False|");
+		System.out.println("________________________________");
+		System.out.println("|  True | True  | False | False|");
+		System.out.println("________________________________");
+		System.out.println("|  True | False | False | False|");
+		System.out.println("--------------------------------");
+	}
+
+}
+	
